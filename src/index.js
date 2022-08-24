a5c1a3eb0ebd1edaf6f6d6b873364626f48e371f
Evans Ochieng <evansochieng121@gmail.com>
2022-08-24 16:34:14 +0300

    Added dog images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('%c HI', 'color: firebrick')
+
+// Challenge 1
+// Add images to the DOM
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetch images
+    fetch(imgUrl)
+    .then( (response) => response.json()) //parse response as json
+    .then( (result) => {
+        imageArray = result.message
+        // for (let element of result){
+        //     addImage(element)
+        // }
+        imageArray.forEach( (element) => addImage(element)) //add image to the DOM
+    })
+})
+
+// Add image function
+function addImage(imageSource){
+    const img = document.createElement('img'); //create an image tag
+    img.src = imageSource; //set the source to be input from the atrrays
+
+    // Grab the div for the images
+    const imageDiv = document.querySelector("#dog-image-container");
+
+    // Append image
+    imageDiv.appendChild(img);
+}
